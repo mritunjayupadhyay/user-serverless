0fb6511527c41fe0dcbaee4c1d4816c2f66a9d45
mritunjayupadhyay <mupadhyay00@gmail.com>
2025-06-23 10:24:10 +0700

    feat: deployed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 // import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SubjectsModule } from './modules/subjects/subjects.module';
 import { DatabaseModule } from './db/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, SubjectsModule, UsersModule],
+  imports: [ConfigModule, DatabaseModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
