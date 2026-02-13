c7c531ad14d751bf6cc59398804f87104a8c7508
mritunjayupadhyay <mupadhyay00@gmail.com>
2026-02-13 11:05:34 +0700

    feat: remove schema

--- src/db/migrations/0003_nosy_scrambler.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE "users" RENAME TO "members";--> statement-breakpoint
-ALTER TABLE "members" DROP CONSTRAINT "users_clerk_id_unique";--> statement-breakpoint
-ALTER TABLE "members" DROP CONSTRAINT "users_email_unique";--> statement-breakpoint
-ALTER TABLE "user_roles" DROP CONSTRAINT "user_roles_user_id_users_id_fk";
---> statement-breakpoint
-ALTER TABLE "user_roles" ADD CONSTRAINT "user_roles_user_id_members_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."members"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "members" ADD CONSTRAINT "members_clerk_id_unique" UNIQUE("clerk_id");--> statement-breakpoint
-ALTER TABLE "members" ADD CONSTRAINT "members_email_unique" UNIQUE("email");
--- src/db/migrations/0004_bumpy_argent.sql
+++
@@ -1,4 +0,0 @@
-CREATE TYPE "public"."gender" AS ENUM('male', 'female', 'other');--> statement-breakpoint
-ALTER TABLE "members" ADD COLUMN "gender" "gender" DEFAULT 'male' NOT NULL;--> statement-breakpoint
-ALTER TABLE "members" ADD COLUMN "profile_pic" varchar;--> statement-breakpoint
-ALTER TABLE "members" ADD COLUMN "birthday" varchar(50);
